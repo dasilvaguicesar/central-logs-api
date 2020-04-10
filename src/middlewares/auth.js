a208bc65580e01bb3b45825312bcb624c93f5f2f
dasilvaguilhermecesar <dasilvaguilhermecesar@gmail.com>
2020-04-10 01:42:34 -0300

    test in signin

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -30,7 +30,8 @@ module.exports = {
         return res.status(401).json({ message: 'Incorrect password' })
       }
     } catch (error) {
-      return res.status(500).json(error)
+      console.log(error)
+      return res.status(500).json({ message: 'Internal server error' })
     }
   },
   
